67e7f9b272ad6976ab769bcf479f20516c2b15f3
gelobltrs-pixel <gelobltrs@gmail.com>
2026-07-28 10:49:54 +0800

    Add files via upload

---
+++ week4.py
@@ -0,0 +1,39 @@
+#maze = {"A": ["B"], "B": ["A", "c"], "C":  ["B", "D"], "D": ["A", "C"]  }
+#start, goal = "A", "D"
+#frontier = maze[START]
+#search_space = list(maze.keys())
+
+#print(frontier)
+#print(search_space)
+#print(goal in frontier)
+from collections import deque
+maze = {
+"A": ["B", "C"],
+"B": ["A", "C", "B"],
+"C": ["D", "E"],
+"D": ["B"],
+"E": ["G"],
+"F": ["D", "F"],
+"G": ["A"],
+}
+
+def bfs_find_maze(maze, start, goal):
+queue = deque([[start]])
+visited = set()
+
+while queue:
+    path = queue.popleft()
+    node = path[-1]
+
+    print("Exploring:", path)
+
+    if node == goal:
+        return path
+
+        if node not in visited:
+            visited.add(node)
+for neighbor in maze[node]:
+    queue.append(path + [neighbor])
+
+    return None
+print(bfs_find_maze(maze, "A", "G"))
